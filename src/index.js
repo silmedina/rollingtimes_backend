961b5a91c9d9b6f084aabd093c46e1ee8739a0ac
sil Medina <medina.silviapatricia@gmail.com>
2021-05-21 02:18:28 -0300

    Se agregaron las configuraciones iniciales

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import path from 'path'
 
 const app = express();
 
-app.set('port', 4000);
+app.set('port', process.env.PORT || 4001);
 
 app.listen(app.get('port'), ()=>{
-    console.log('Estoy en el puerto ' + app.get('port'))
+    console.log('Listening on port: ' + app.get('port'))
 });
 
+// Middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));  
+app.use(express.static(path.join(__dirname, '../public')));  
+
+// Rutas
+app.get('/', (request, response)=>{
+    response.send('Response inicial');
+})
